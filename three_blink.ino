dee2194703b1101210684d2d5c48c9319f8bfd2e
ToThyMoon <112587293+ToThyMoon@users.noreply.github.com>
2024-02-15 11:24:41 -0500

    Added sample code
    Added code for the required assignment

---
+++ three_blink.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(digitalRead(9)){
+    digitalWrite(5, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(500);            // wait for a second
+    digitalWrite(5, LOW);   // turn the LED off by making the voltage LOW
+    digitalWrite(6, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(500);            // wait for a second
+    digitalWrite(6, LOW);   // turn the LED off by making the voltage LOW
+    digitalWrite(7, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(500);            // wait for a second
+    digitalWrite(7, LOW);   // turn the LED off by making the voltage LOW
+  }
+}
